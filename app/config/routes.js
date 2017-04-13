1ddd7816aa7f28ed50004fd986f91c34081049ca
Mango95 <nmhai1995@gmail.com>
2017-04-14 01:40:23 +0700

    Add about feature

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,5 +1,6 @@
 var albumsController = require('../controllers/albumsController');
 var imagesController = require('../controllers/imagesController');
+var aboutController = require('../controllers/aboutController');
 
 var configRoutes = function(app){
   app.get('/', function(req, res){
@@ -9,6 +10,7 @@ var configRoutes = function(app){
   app.get('/albums/:id', imagesController.showImages);
   app.get('/images/:id', imagesController.showImage);
   app.get('/image/:id', imagesController.getImage);
+  app.get('/about', aboutController.index);
 };
 
 module.exports = configRoutes;
