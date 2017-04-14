26260c444e7a3022472200270f33c09f845fe7ed
Mango95 <nmhai1995@gmail.com>
2017-04-15 00:31:40 +0700

    Add Breadcrumb to About

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,3 +1,4 @@
+var aboutController = require('../controllers/aboutController');
 var albumsController = require('../controllers/albumsController');
 var imagesController = require('../controllers/imagesController');
 var blogsController = require('../controllers/blogsController');
@@ -6,6 +7,7 @@ var configRoutes = function(app){
   app.get('/', function(req, res){
     res.render('layout');
   });
+  app.get('/about', aboutController.index);
   app.get('/albums', albumsController.getAllAlbums);
   app.get('/albums/:id', imagesController.showImages);
   app.get('/images/:id', imagesController.showImage);
